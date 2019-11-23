88a84e1b15eb2c9473177f1dc98e3d21dd245808
Paragk05 <paragk106@gmail.com>
2019-11-23 16:36:55 +0530

    Chat Box Using Socket.io Node js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express= require('express');
+const socket= require('socket.io');
+
+
+//App setup
+
+const app=express();
+
+const server= app.listen(4000,function()
+{
+    console.log('listening to request on port 4000');
+});
+
+//Static files
+
+app.use(express.static('public'));
+
+//Socket setup
+
+const io=socket(server);
+
+
+io.on('connection',function(socket){
+
+    console.log('made socket connection '+socket.id);
+
+    //Handle Chat Event
+    socket.on('chat',function(data){
+
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',function(data)
+    {
+        socket.broadcast.emit('typing',data);
+    })
+});
